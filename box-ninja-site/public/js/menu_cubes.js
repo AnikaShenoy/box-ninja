9c4add3ca04cc0b6f917f96f85b536e9a8b30150
AnikaShenoy <anika.shenoy@mail.utoronto.ca>
2020-12-31 17:23:46 -0500

    responsive design

--- box-ninja-site/public/js/menu_cubes.js
+++ box-ninja-site/public/js/menu_cubes.js
@@ -1,11 +1,15 @@
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/build/three.module.js';
+
+const container = document.querySelector("#flex-container");
+
 
 //set up scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("black");
 const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
+renderer.setSize( container.clientWidth, container.clientHeight);
 camera.position.z=5;
 
 
