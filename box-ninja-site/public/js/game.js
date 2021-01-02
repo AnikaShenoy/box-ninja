c59df4464a27a8c82d6b1f6c4ab415c7960e4b3a
Natalie Volk <natalievolk@rogers.com>
2021-01-02 11:26:24 -0500

    randomize box colours, location, timing

--- box-ninja-site/public/js/game.js
+++ box-ninja-site/public/js/game.js
@@ -1,5 +1,3 @@
-
-
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/build/three.module.js';
 
 function main() {
@@ -15,18 +13,16 @@ function main() {
     // create a new scene
     var scene = new THREE.Scene();
 
-    {
-        // creating a directional light to give box definition
-        const color = 0xFFFFFF;
-        const intensity = 1;
-        const light = new THREE.DirectionalLight(color, intensity);
+    // creating a directional light to give box definition
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
 
-        // directional lights have a position and target --> default of both is (0,0,0)
-        // setting position to (-1,2,4) --> left, above, and behind our camera
-        // leaving target as default (so it will shine to origin)
-        light.position.set(-1, 2, 4);
-        scene.add(light);
-    }
+    // directional lights have a position and target --> default of both is (0,0,0)
+    // setting position to (-1,2,4) --> left, above, and behind our camera
+    // leaving target as default (so it will shine to origin)
+    light.position.set(-1, 2, 4);
+    scene.add(light);
 
     // PerspectiveCamera mimics the way humans see a scene
     // 75 --> FOV (in degrees)
@@ -59,25 +55,45 @@ function main() {
 
     // creating three cubes with three different colours:
     var geometry = new THREE.BoxGeometry(1, 1, 1);
-    const cubes = [
-        makeShape(geometry, 0x44aa88, [0, 0]),
+    var cubes = [
+        makeShape(geometry, 0x44aa88, [0,0]),
         makeShape(geometry, 0x8844aa, [-2, 2]),
-        makeShape(geometry, 0xaa8844, [2,0])
+        makeShape(geometry, 0xaa8844, [2,0]),
+        makeShape(geometry, 0x4deeea, [1, 1]),
+        makeShape(geometry, 0x74ee15, [5,2])
     ];
-
+    
+    
     // this creates a loop that will keep on rendering the app everytime the page refreshes
     function render(time) { // time is given in milliseconds (ms)
         time *= 0.001 // convert time from milliseconds to seconds
 
+        if (Math.floor(time) % 2 == 0) {
+            var cube = cubes[Math.floor(Math.random() * 5)];
+            console.log(cubes);
+            cube.translateZ(0.1);
+        }
+
+        if (Math.random() < 0.1) {
+            var cube = cubes[Math.floor(Math.random() * 5)];
+            cube.translateZ(0.1);
+        }
+        
+        /*
         cubes.forEach((cube, index) => {  // => is just a way to shorten the syntax
             // change the speed for each cube
-            const speed = 1 + index * 0.1; 
-            const rot = time * speed;
-            cube.rotation.x = rot;
-            cube.rotation.y = rot;
-            cube.translateY(0.07);
-            //cube.translateZ(0.07);
+            
+            const speed = 0.1 + index * 0.05; 
+            
+            const rot = time * 0.5;
+            //cube.rotation.x = rot;
+            //cube.rotation.y = rot;
+            //cube.rotation.z = rot;
+            
+            //cube.translateY(speed);
+            cube.translateZ(speed);
         });
+        */
         
         // this actually draws the scene
         renderer.render(scene, camera);
@@ -85,7 +101,112 @@ function main() {
         requestAnimationFrame(render);
     }
     requestAnimationFrame(render);
+}
 
-    }
-
-main();
+main();     
+          
+          
+          
+          
+          
+          /*
+            // !!! THE BELOW CODE IS COPY-PASTED (and slightly edited) FROM:
+            //      https://threejs.org/docs/index.html#api/en/core/Raycaster
+            // currently doesn't work though
+            const raycaster = new THREE.Raycaster();
+            const mouse = new THREE.Vector2();
+        
+            function onMouseMove( event ) {
+        
+                // calculate mouse position in normalized device coordinates
+                // (-1 to +1) for both components
+        
+                mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+                mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+        
+            }
+        
+            function render() {
+        
+                // update the picking ray with the camera and mouse position
+                raycaster.setFromCamera( mouse, camera );
+        
+                // calculate objects intersecting the picking ray
+                const intersects = raycaster.intersectObjects(cubes);
+        
+                for ( let i = 0; i < intersects.length; i ++ ) {
+        
+                    intersects[ i ].object.material.color.set( 0xff0000 );
+        
+                }
+        
+                renderer.render( scene, camera );
+        
+            }
+        
+            window.addEventListener( 'mousemove', onMouseMove, false );
+        
+            requestAnimationFrame(render)
+        
+            }
+            */
+        
+            /*
+            // !! COPIED FROM:
+            //      https://riptutorial.com/three-js/example/17088/object-picking---raycasting
+            // but doesn't work 
+            
+            function raycast ( e ) {
+            // Step 1: Detect light helper
+                //1. sets the mouse position with a coordinate system where the center
+                //   of the screen is the origin
+                mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+                mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+        
+                //2. set the picking ray from the camera position and mouse coordinates
+                raycaster.setFromCamera( mouse, camera );    
+        
+                //3. compute intersections (note the 2nd parameter)
+                var intersects = raycaster.intersectObjects( scene.children, true );
+        
+                for ( var i = 0; i < intersects.length; i++ ) {
+                    console.log( intersects[ i ] ); 
+                    
+                        An intersection has the following properties :
+                            - object : intersected object (THREE.Mesh)
+                            - distance : distance from camera to intersection (number)
+                            - face : intersected face (THREE.Face3)
+                            - faceIndex : intersected face index (number)
+                            - point : intersection point (THREE.Vector3)
+                            - uv : intersection point in the object's UV coordinates (THREE.Vector2)
+                    
+                }
+            // Step 2: Detect normal objects
+                //1. sets the mouse position with a coordinate system where the center
+                //   of the screen is the origin
+                mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+                mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+        
+                //2. set the picking ray from the camera position and mouse coordinates
+                raycaster.setFromCamera( mouse, camera );    
+        
+                //3. compute intersections (no 2nd parameter true anymore)
+                var intersects = raycaster.intersectObjects( scene.children );
+        
+                for ( var i = 0; i < intersects.length; i++ ) {
+                    console.log( intersects[ i ] ); 
+                    intersects[ i ].object.material.color.set( 0xff0000 );
+                    
+                        An intersection has the following properties :
+                            - object : intersected object (THREE.Mesh)
+                            - distance : distance from camera to intersection (number)
+                            - face : intersected face (THREE.Face3)
+                            - faceIndex : intersected face index (number)
+                            - point : intersection point (THREE.Vector3)
+                            - uv : intersection point in the object's UV coordinates (THREE.Vector2)
+                    
+                }
+        
+                raycaster = new THREE.Raycaster();
+                renderer.domElement.addEventListener( 'click', raycast, false );
+            */
