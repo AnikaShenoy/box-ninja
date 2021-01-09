e697a57ebc36090941b0db7ec783ce154cc836bb
AnikaShenoy <anika.shenoy@mail.utoronto.ca>
2021-01-09 02:29:14 -0500

    add react script tags

--- box-ninja-site/src/components/Home.js
+++ box-ninja-site/src/components/Home.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React from 'react';
 /* import threeEntryPoint from "./threejs/threeEntryPoint"; */
 import "../App.css"
-
+import ScriptTag from "react-script-tag";
 
 
 /* 
@@ -25,11 +25,15 @@ const home = () => {
     return (
        <div>
             <h1 id="title">Box Ninja</h1>
-            <video autoplay="false" id="videoElement"></video>
+            {/* <video autoplay="false" id="videoElement"></video> */}
 
-            <img id="hand" src="images/hand.png"/>
+            <img id="hand" src="images/hand.png" alt=""/>
 
-            
+            <div className="outer">
+                <canvas id="flex-container"></canvas>
+            </div>
+            <ScriptTag type="text/javascript" src=".\MenuCubes.js"></ScriptTag>
+
        </div>
     );
 }
