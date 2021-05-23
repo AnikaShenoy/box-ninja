11d9f3ccc377b578b6da31ceb5d4b5fe4587bd3a
Natalie Volk <natalievolk@rogers.com>
2021-05-23 14:42:23 -0400

    fix page linking

--- box-ninja-site/src/components/Home.jsx
+++ box-ninja-site/src/components/Home.jsx
@@ -2,6 +2,26 @@ import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 import "../App.css";
 
+const mouseLinkStyle = {
+    //padding: "0.5em",
+    position: "relative",
+    left: "350px",
+    top: "00px",
+    //color: rgba(255, 255, 255, 0.75),
+    opacity: 1,
+    //font-family: "Bungee", Bungee Shade
+};
+
+const gestureLinkStyle = {
+    //padding: "0.5em",
+    position: "relative",
+    left: "800px",
+    top: "00px",
+    //color: rgba(255, 255, 255, 0.75),
+    opacity: 1,
+    //font-family: "Bungee", Bungee Shade
+};
+
 export default class Home extends Component {
     
     componentDidMount() {
@@ -24,6 +44,7 @@ export default class Home extends Component {
         
     }
 
+
     render() {
         return (
             
@@ -31,8 +52,21 @@ export default class Home extends Component {
                 <h1 id="title">Box Ninja</h1>
                 <h3 id="points"></h3>
 
-                <Link id="mouseLink" to='/game-mouse'> Mouse Control </Link>
-                <Link id="gestureLink" to='/game-gesture'> Hand Control </Link>
+                {/* <Link id="mouseLink" to='/game-mouse'> Mouse Control </Link>
+                <Link id="gestureLink" to='/game-gesture'> Hand Control </Link> */}
+
+                {/* <Link id="mouseLink" to='/game-mouse'> Mouse Control </Link>
+                <button id="gestureLink" to='/game-gesture'> Hand Control </button> */}
+
+                {/* <ul>
+                    <li><Link to='/game-mouse' style={mouseLinkStyle}> Mouse Control </Link></li>
+                    <li><Link to='/game-gesture' style={gestureLinkStyle}> Hand Control </Link></li>
+                </ul> */}
+
+                <ul>
+                    <li><Link id="mouseLink" to='/game-mouse'> Mouse Control </Link></li>
+                    <li><Link id="gestureLink" to='/game-gesture'> Hand Control </Link></li>
+                </ul>
 
                 <canvas id="flex-container"></canvas>
 
